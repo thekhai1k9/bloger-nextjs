22ab1afaf84a1af30cd2d3c5937d5212f5105a9f
thekhai1k9 <thekhai1k9@gmail.com>
2026-05-17 14:58:06 +0700

    uopdate master

--- src/components/Layouts/MainLayout/Footer.tsx
+++ src/components/Layouts/MainLayout/Footer.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Layout from '../Layout'
 import Link from 'next/link'
-import {QuoteBox} from '@/components/QuoteBox'
 
 interface FooterIProps {
     className?: string
@@ -12,7 +11,6 @@ const Footer:React.FC<FooterIProps> = ({className}) => {
     <footer className='w-full border-t-2 border-solid border-dark font-medium text-lg dark:text-light dark:border-light
       sm:text-base'
     >
-      <QuoteBox />
       <Layout className='py-8 flex items-center justify-between lg:flex-col lg:py-6'>
         <span>{new Date().getFullYear()} &copy; All Rights Reserved</span>
         <div className='flex items-center lg:py-2'>
